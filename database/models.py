74c45ab37c55e78491c2c3f66d37422204cde15e
jabyum <jabyum@github.com>
2024-12-12 21:46:20 +0500

    first

---
+++ database/models.py
@@ -0,0 +1,45 @@
+from database import Base
+from sqlalchemy import (Column, Integer, String,
+                        DateTime, Boolean, ForeignKey)
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
+# создание моделей
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False, unique=True)
+    level = Column(String, default="easy")
+    reg_date = Column(DateTime, default=datetime.now())
+# вопросы
+class Question(Base):
+    __tablename__ = "questions"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    main_question = Column(String, nullable=False) # на самом деле nullable по умолчанию False
+    v1 = Column(String, nullable=False)
+    v2 = Column(String, nullable=False)
+    v3 = Column(String, nullable=True)
+    v4 = Column(String, nullable=True)
+    correct_answer = Column(Integer)
+    # timer = Column(Integer, default=45)
+# регистрация ответов на каждый вопрос
+class UserAnswer(Base):
+    __tablename__ = "useranswers"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    question_id = Column(Integer, ForeignKey("questions.id"))
+    # user_answer = Column(String, nullable=True)
+    user_answer = Column(Integer)
+    correctness = Column(Boolean, default=False)
+    level = Column(String)
+    # создаем связь с другими таблицами в виде подзапроса
+    user_fk = relationship(User, lazy="subquery")
+    question_fk = relationship(Question, lazy="subquery")
+
+
+
+
+
+
+
